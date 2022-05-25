f13f3450792adb5d3c3b8ad510f624c5af64460b
Rasel Kabir <rasel.kabir@shiplux.com>
2022-05-25 11:44:09 +0600

    Update Leeson 22 break and continue

--- L22 uses break and continue/index.js
+++ L22 uses break and continue/index.js
@@ -1,1 +1,42 @@
 // Uses break and continue
+
+// Break
+for (var i = 0; i <= 100; i++) {
+    if (i == 10) {
+        break
+    }
+
+    document.write(" " + i)
+}
+
+document.write("<br>" + "End" + "<br>")
+
+// Continue
+for (var i = 0; i <= 110; i++) {
+    if (i == 100) {
+        continue
+    }
+
+    document.write(" " + i)
+}
+
+document.write("<br>" + "End" + "<br>")
+
+// Continue
+for (var i = 0; i <= 110; i++) {
+    if (i % 2 == 0) {
+        continue
+    }
+
+    document.write(" " + i)
+}
+document.write("<br>" + "End" + "<br>")
+
+// Continue
+for (var i = 1; i <= 110; i++) {
+    if (i == 20) {
+        break
+    }
+
+    document.write(" " + i)
+}
