a8637275c7bd11583a3e4fa9eefa539fd6f9a439
Rasel Kabir <rasel.kabir@shiplux.com>
2022-05-26 13:15:06 +0600

    Done Lesson 24 and 25

---
+++ L 25 WEFs and function expression/index.js
@@ -0,0 +1,23 @@
+// IIFEs (Immediately Invokeable function Expressoins)
+
+// Task 7: Create and IIFEs That Print sum of 2 number
+/*var num1 = parseFloat(prompt(" Enter Number one : "));
+var num2 = parseFloat(prompt(" Enter Number two : "));
+
+
+(function display(Message) {
+    var Message = num2 + num1;
+    console.log(Message);
+})(); */
+
+// Function Expressions
+// (), {}, [], <>
+
+var display2 = function (Message) {
+    console.log(Message)
+};
+display2(" Hello this is rasel kabir")
+
+
+// Task 7: Create and IIFEs That Print sum of 2 number
+
