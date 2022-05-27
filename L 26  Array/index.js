bc504fa42bb8255050eb5a6019a13c87e90fe88a
Rasel Kabir <rasel.kabir@shiplux.com>
2022-05-28 00:03:19 +0600

    Array L 26

---
+++ L 26  Array/index.js
@@ -0,0 +1,19 @@
+// Array is an Objext, collection of variables
+
+var name1, name2, name3, name4, name5
+name1 = "Anis";
+name2 = "Lima";
+name3 = "Shipa";
+name4 = "Renku";
+name5 = "tikku";
+
+var names = new Array();
+names[0] = "anis";
+names[1] = "Lima";
+names[2] = "Shipa";
+names[3] = "Renku";
+names[4] = "tikku";
+names[5] = "Rasel";
+
+console.log(names);
+
