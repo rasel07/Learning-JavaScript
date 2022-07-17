df775c0693969ad1594878db0cf4f2a3596d737b
Rasel Kabir <rasel.kabir@shiplux.com>
2022-07-17 13:57:38 +0600

    Lesson 55

--- L 55 DOM Event Object _Browser_object Model_Location object/index.js
+++ L 55 DOM Event Object _Browser_object Model_Location object/index.js
@@ -1,26 +1,41 @@
-// DragEvent Object
+// BOM  (Browser Object Model)
+// window object
+// location object
+console.clear()
+//console.log(location)
+// href
+//console.log(location.href)
 
-//ondragstart
-//ondrag
-//ondragend
-//ondragenter
-//ondragleave
-//ondragover
-// ondrop
+// protocol
+//console.log(location.protocol)
 
-const box = document.querySelector("#div1");
-const textp = document.querySelector("#drag1")
+// hostneme
+//console.log(location.hostname)
 
-textp.addEventListener('dragstart', function (e) {
-    e.dataTransfer.setData("text", e.target.id)
-});
+// Port
+//console.log(location.port)
 
-box.addEventListener("dragover", function (e) {
-    e.preventDefault()
-});
+// Path Name
+//console.log(location.pathname)
 
-box.addEventListener("drop", function (e) {
-    let id = e.dataTransfer.getData("text");
-    box.appendChild(document.getElementById(id));
-    e.preventDefault()
+let locationDiv = document.querySelector(".location-div");
+var p1 = locationDiv.children[0]
+p1.textContent = location.href;
+
+var p2 = locationDiv.children[1]
+p2.textContent = location.hostname;
+
+var p3 = locationDiv.children[2]
+p3.textContent = location.protocol;
+
+var p4 = locationDiv.children[3]
+p4.textContent = location.port;
+
+var p5 = locationDiv.children[4]
+p5.textContent = location.pathname;
+
+const visitMY = document.querySelector("#visited");
+
+visitMY.addEventListener("click", function () {
+    location.assign("https://raselkabir.me")
 })
